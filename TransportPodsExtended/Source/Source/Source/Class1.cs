082a7f2afcecc08786602c832361879370811746
Mersid <30786211+Mersid@users.noreply.github.com>
2018-03-29 02:10:05 -0700

    Add files via upload

---
+++ TransportPodsExtended/Source/Source/Source/Class1.cs
@@ -0,0 +1,117 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+using UnityEngine;
+
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Harmony;
+
+namespace TransportPodsExtended
+{
+
+	[StaticConstructorOnStartup]
+	public class TPE_HarmonyPatch
+	{
+		static TPE_HarmonyPatch()
+		{
+			HarmonyInstance harmony = HarmonyInstance.Create("TransportPod");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+			var methods = harmony.GetPatchedMethods();
+
+			foreach (var method in methods)
+			{
+				Log.Warning(method.ToString());
+			}
+
+		}
+	}
+
+	[HarmonyPatch(typeof(CompLaunchable))]
+	[HarmonyPatch("MaxLaunchDistanceAtFuelLevel")]
+	static class MaxDistancePatch
+	{
+		static void Postfix(ref int __result, float fuelLevel)
+		{
+			__result = Mathf.FloorToInt(fuelLevel / 10f);
+		}
+	}
+	
+	[HarmonyPatch(typeof(CompLaunchable))]
+	[HarmonyPatch("TryLaunch")]
+	static class TryLaunchPatch
+	{
+
+		static HashSet<LaunchableData> podData;
+
+		static TryLaunchPatch()
+		{
+			
+		}
+
+		static void Prefix(CompLaunchable __instance, GlobalTargetInfo target, PawnsArriveMode arriveMode, bool attackOnArrival)
+		{
+
+			podData = new HashSet<LaunchableData>(__instance.TransportersInGroup.Where(ct => ct.Launchable.FuelingPortSource != null).GroupBy(ct => ct.Launchable.FuelingPortSource.TryGetComp<CompRefuelable>()).Select(grp => new LaunchableData(grp.Key, grp.Sum(ct => ct.GetDirectlyHeldThings().Sum(t => t.GetStatValue(StatDefOf.Mass))), grp.Key.Fuel, grp.Count())));
+
+
+			for (int i = 0; i < __instance.TransportersInGroup.Count; i++)
+			{
+
+				Map map = __instance.parent.Map;
+				int TraversalDistance = Find.WorldGrid.TraversalDistanceBetween(map.Tile, target.Tile);
+
+				CompTransporter compTransporter = __instance.TransportersInGroup[i];
+				Building fuelingPortSource = compTransporter.Launchable.FuelingPortSource;
+				if (fuelingPortSource != null)
+				{
+					fuelingPortSource.TryGetComp<CompRefuelable>().ConsumeFuel((float) (TraversalDistance * (2 + GetTotalMass() / (150 * __instance.TransportersInGroup.Count)) - (2.25 * TraversalDistance)));
+				}
+
+			}
+
+		}
+
+		static void Postfix(CompLaunchable __instance, GlobalTargetInfo target, PawnsArriveMode arriveMode, bool attackOnArrival)
+		{
+			
+		}
+
+		static float GetTotalMass()
+		{
+			float mass = 0;
+
+			foreach (LaunchableData data in podData)
+			{
+				mass += data.weight;
+			}
+
+			return mass;
+		}
+
+		public struct LaunchableData
+		{
+			public CompRefuelable refuelable;
+			public float weight;
+			public float initialFuel;
+			public int podCount;
+
+			public LaunchableData(CompRefuelable refuelable, float weight, float initialFuel, int podCount)
+			{
+				this.refuelable = refuelable;
+				this.weight = weight;
+				this.initialFuel = initialFuel;
+				this.podCount = podCount;
+			}
+		}
+
+	}
+
+}
+*/
---
+++ TransportPodsExtended/Source/Source/Source/Mod.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using UnityEngine;
+
+using RimWorld;
+using Verse;
+using Harmony;
+
+namespace TransportPodsExtended
+{
+	// Loads Harmony
+	[StaticConstructorOnStartup]
+	public class TPE_HarmonyPatch
+	{
+		static TPE_HarmonyPatch()
+		{
+			HarmonyInstance harmony = HarmonyInstance.Create("TransportPod");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+			var methods = harmony.GetPatchedMethods();
+
+			foreach (var method in methods)
+			{
+				Log.Warning(method.ToString()); // Lists each loaded patch in this mod
+			}
+
+		}
+	}
+	static class LaunchableMethods
+	{
+		public static float FuelNeededToLaunchAtDist(float dist, CompLaunchable launchable)
+		{
+			return FuelNeededToLaunchAtDist(dist, PercentFull(launchable));
+		}
+		public static float FuelNeededToLaunchAtDist(float dist, float percentFull)
+		{
+			float ret = (MassFactor(percentFull) * dist);
+			return ret;
+		}
+
+		public static int MaxLaunchDistanceAtFuelLevel(float fuelLevel, CompLaunchable launchable)
+		{
+			return MaxLaunchDistanceAtFuelLevel(fuelLevel, PercentFull(launchable));
+		}
+
+		public static int MaxLaunchDistanceAtFuelLevel(float fuelLevel, float percentFull)
+		{
+			int ret = Mathf.FloorToInt(fuelLevel / MassFactor(percentFull));
+			return ret;
+		}
+
+		public static float MassFactor(float percentFull)
+		{
+			return (0.6f + 2.0f * percentFull);
+		}
+
+		public static float PercentFull(CompLaunchable launchable)
+		{
+			float mass = 0;
+			List<CompTransporter> transporters = launchable.TransportersInGroup;
+			foreach (CompTransporter transporter in transporters)
+			{
+				foreach (Thing t in transporter.GetDirectlyHeldThings())
+				{
+					mass += t.GetStatValue(StatDefOf.Mass);
+				}
+			}
+			float max = transporters.Count * launchable.parent.GetStatValue(StatDefOf.CarryingCapacity);
+			return mass / (max * (150 / launchable.parent.GetStatValue(StatDefOf.CarryingCapacity)));
+		}
+	}
+
+
+	[HarmonyPatch(typeof(CompLaunchable), "MaxLaunchDistanceAtFuelLevel")]
+	public static class MaxLaunchDistanceAtFuelLevelPatch
+	{
+		public static void Postfix(float fuelLevel, ref int __result)
+		{
+			__result = LaunchableMethods.MaxLaunchDistanceAtFuelLevel(fuelLevel, 0f);
+		}
+	}
+
+	[HarmonyPatch(typeof(CompLaunchable), "TryLaunch")]
+	//private void TryLaunch(GlobalTargetInfo target, PawnsArriveMode arriveMode, bool attackOnArrival)
+	public static class TryLaunchPatch
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+		{
+			MethodInfo FuelNeededToLaunchAtDistInfo = AccessTools.Method(typeof(CompLaunchable), "FuelNeededToLaunchAtDist");
+			MethodInfo FuelNeededToLaunchAtDistInfoPatch = AccessTools.Method(typeof(LaunchableMethods), "FuelNeededToLaunchAtDist",
+				new Type[] { typeof(float), typeof(CompLaunchable) });
+
+			foreach (CodeInstruction i in codeInstructions)
+			{
+				if (i.opcode == OpCodes.Call && i.operand == FuelNeededToLaunchAtDistInfo)
+				{
+					i.operand = FuelNeededToLaunchAtDistInfoPatch;
+					yield return new CodeInstruction(OpCodes.Ldarg_0); //this
+				}
+				yield return i;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(CompLaunchable), "ChoseWorldTarget")]
+	//private void TryLaunch(GlobalTargetInfo target, PawnsArriveMode arriveMode, bool attackOnArrival)
+	static class ChoseWorldTargetPatch
+	{
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+		{
+			return TryLaunchPatch.Transpiler(codeInstructions);
+		}
+	}
+
+	[HarmonyPatch(typeof(CompLaunchable), "get_MaxLaunchDistance")]
+	//private void TryLaunch(GlobalTargetInfo target, PawnsArriveMode arriveMode, bool attackOnArrival)
+	public static class MaxLaunchDistance_Patch
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+		{
+			MethodInfo MaxLaunchDistanceAtFuelLevel = AccessTools.Method(typeof(CompLaunchable), "MaxLaunchDistanceAtFuelLevel");
+			MethodInfo MaxLaunchDistanceAtFuelLevelPatch = AccessTools.Method(typeof(LaunchableMethods), "MaxLaunchDistanceAtFuelLevel",
+				new Type[] { typeof(float), typeof(CompLaunchable) });
+
+			foreach (CodeInstruction i in codeInstructions)
+			{
+				if (i.opcode == OpCodes.Call && i.operand == MaxLaunchDistanceAtFuelLevel)
+				{
+					i.operand = MaxLaunchDistanceAtFuelLevelPatch;
+					yield return new CodeInstruction(OpCodes.Ldarg_0); //this
+				}
+				yield return i;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(CompLaunchable), "get_MaxLaunchDistanceEverPossible")]
+	//private void TryLaunch(GlobalTargetInfo target, PawnsArriveMode arriveMode, bool attackOnArrival)
+	static class MaxLaunchDistanceEverPossible_Patch
+	{
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+		{
+			return MaxLaunchDistance_Patch.Transpiler(codeInstructions);
+		}
+	}
+}
